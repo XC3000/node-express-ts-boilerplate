f121676295a55860453bfcd8dd8955057a1a5dd1
richard-jhavtech <richard.rozario@jhavtech.com>
2023-09-10 14:25:01 +0530

    init

--- src/utils/assertIsDefined.ts
+++
@@ -1,7 +0,0 @@
-export default function assertIsDefined<T>(
-  val: T
-): asserts val is NonNullable<T> {
-  if (val === null || val === undefined) {
-    throw Error("Expected 'val' to be defined but received " + val);
-  }
-}
